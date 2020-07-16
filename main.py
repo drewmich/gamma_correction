1f22e89271139281ca9416a9cfbd365d70e06141
Drew Michelini <dbmich@umich.edu>
2020-07-16 12:41:44 -0400

    User now provides the input and output path via the command line. Added another argument that allows to select for contrast limited adaptive histogram equalization, or a manual gamma value that is still hardcoded in

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
-from scipy import ndimage, misc
+#!/usr/bin/env python
 import imageio
 import numpy as np
 import os
 import cv2 as cv
+import sys
 
 
 def main():
 
-    gamma = 0.5
+
 
     lookUpTable = np.empty((1, 256), np.uint8)
 
-    outPath = "/Users/drewmichelini/Images/output"
-    path = "/Users/drewmichelini/Images/input/"
+    outPath = sys.argv[1]
+    #"/Users/drewmichelini/Images/output"
+    path = sys.argv[2]
+    #"/Users/drewmichelini/Images/input/"
 
     # iterate through the names of contents of the folder
     for image_path in os.listdir(path):
 
-        # create the full input path and read the file
+        # make the input path and read the file
         input_path = os.path.join(path, image_path)
-        img_original = imageio.imread(input_path)
+        img = imageio.imread(input_path)
 
-        for i in range(256):
-            lookUpTable[0, i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
-        res = cv.LUT(img_original, lookUpTable)
+        if (sys.argv[3] == "clahe"):
+            # convert to grayscale
+            gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-        # create full output path, 'example.jpg'
-        # becomes 'adjusted_example.jpg', save the file to disk
-        fullpath = os.path.join(outPath, 'adjusted_' + image_path)
-        imageio.imwrite(fullpath, res)
-        #(255.0 * img_original).astype('uint8')
+            # create a CLAHE object (Arguments are optional).
+            clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+            cl1 = clahe.apply(gray_image)
+
+            fullpath = os.path.join(outPath, 'adjusted_' + image_path)
+            imageio.imwrite(fullpath, cl1)
+
+        if (sys.argv[3] != "clahe"):
+
+            gamma = .95
+            for i in range(256):
+                lookUpTable[0, i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
+            res = cv.LUT(img, lookUpTable)
+
+            # makes path for output
+            fullpath = os.path.join(outPath, 'adjusted_' + image_path)
+            imageio.imwrite(fullpath, res)
+
 
 if __name__ == '__main__':
     main()
